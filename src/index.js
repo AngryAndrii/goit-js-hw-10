0809a51ee321b4629a821c923d257b3a107459bb
Andrii Chychkan <mywork1728@gmail>
2023-07-02 20:17:23 +0300

    added slim-select

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 'use strict';
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+import '/node_modules/slim-select/dist/slimselect.css';
 // import axios from 'axios';
 
 // axios.defaults.headers.common['x-api-key'] =
-//   'live_vpvUjSl35zCQrr2xVBQkxMWUrItKUIxcePkJ0efHivlt5bmWOjY1RonXbknluLML';
+// 'live_vpvUjSl35zCQrr2xVBQkxMWUrItKUIxcePkJ0efHivlt5bmWOjY1RonXbknluLML';
 
 const selectBreedElem = document.querySelector('.breed-select');
 const catInfoContainer = document.querySelector('.cat-info');
-
-const photoDiv = document.querySelector('.cat-photo');
-const nameDiv = document.querySelector('.full-name');
-const tempDiv = document.querySelector('.temperament');
 
 fetchBreeds().then(data => {
   selectBreedElem.insertAdjacentHTML(
@@ -23,29 +21,32 @@ fetchBreeds().then(data => {
       `<option value="${el.value}">${el.label}</option>`
     );
   });
+  new SlimSelect({
+    select: selectBreedElem,
+  });
 });
 
 selectBreedElem.addEventListener('change', function (event) {
   fetchCatByBreed(event.target.value).then(data => {
     console.log(data);
-    photoDiv.innerHTML = '';
-    photoDiv.insertAdjacentHTML(
-      'beforeend',
-      `<img
-        src="${data[0].url}"
-        alt=""
-        width="450px"
-      />`
-    );
-    nameDiv.innerHTML = '';
-    nameDiv.insertAdjacentHTML(
-      'beforeend',
-      `<p class="ppp">${data[0].breeds[0].name}</p>`
-    );
-    tempDiv.innerHTML = '';
-    tempDiv.insertAdjacentHTML(
-      'beforeend',
-      `<p class="ppp">${data[0].breeds[0].temperament}</p>`
-    );
+    let dataObj = {
+      url: data[0].url,
+      name: data[0].breeds[0].name,
+      temperament: data[0].breeds[0].temperament,
+    };
+    makeElem(dataObj);
   });
 });
+
+const makeElem = ({ url, name, temperament }) => {
+  let card = `<div class="cat-photo"><img
+     src="${url}"
+     alt=""
+     width="450px"
+   /></div>
+      <div class="full-name">${name}</div>
+      <div class="temperament">
+        ${temperament}
+      </div>`;
+  catInfoContainer.innerHTML = card;
+};
