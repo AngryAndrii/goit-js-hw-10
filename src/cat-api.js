380db92569305f8676aac3a52de3eea5ecfbb5b1
Andrii Chychkan <mywork1728@gmail>
2023-07-02 18:53:05 +0300

    finished main logic

---
+++ src/cat-api.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const urlForBreeds = 'https://api.thecatapi.com/v1/breeds';
+const urlForOneBreed = 'https://api.thecatapi.com/v1/images/search?breed_ids=';
+
+const options = {
+  headers: {
+    'x-api-key':
+      'live_vpvUjSl35zCQrr2xVBQkxMWUrItKUIxcePkJ0efHivlt5bmWOjY1RonXbknluLML',
+  },
+};
+
+function fetchBreeds() {
+  return fetch(urlForBreeds, options)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      return data.map(({ id, name }) => ({
+        value: id,
+        label: name,
+      }));
+    })
+    .catch(err => {
+      console.log('Failure', err);
+    });
+}
+
+//------------------------------------------------------------------------------------------
+
+function fetchCatByBreed(breedId) {
+  return fetch(`${urlForOneBreed}${breedId}`, options)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    })
+    .catch(err => {
+      console.log('Failure', err);
+    });
+}
+
+//---------------------------------------------------------------------------------------------
+
+export { fetchBreeds, fetchCatByBreed };
